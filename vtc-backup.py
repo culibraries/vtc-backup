d4cc2dc03633f9a67311fb484f289a5c719bcb40
Fred Schumacher <fwschumacher@gmail.com>
2019-12-19 17:50:55 +0000

    Change to multipart upload

--- vtc-backup.py
+++ vtc-backup.py
@@ -7,46 +7,81 @@
 # Revision History
 # -------------------------------------------------------------------
 # 1.0 2019-02-12 FS Original version
-# 2.0 2019-05-17 FS Updated code structure
+# 2.0 2019-05-17 FS Update code structure
+# 3.0 2019-12-19 FS Change to multipart upload
 
 import boto3
 from datetime import date
 import glob
-import os
+import io, os, sys
 
-VTC_BACKUP = 'C:\\Users\\Public\\Documents\\TimeClock Data\\Backup\\'
+UPLOAD_PART_SIZE = 5 * 1024 * 1024
 
 def copy_backup_to_s3(f):
 
-    # Using aws boto library, instantiate a bucket object and stream
-    # VTC backup file to the bucket
-    #
-    # If an I/O error is encountered, invoke sns message which will
-    # be routed to libnotify@colorado.edu
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket('cubl-vtc')
+    # Do a multipart upload.
     try:
-        with open(f, 'rb') as data:
-            bucket.upload_fileobj(data, os.path.basename(f))
-    except IOError:
+        
+        client = boto3.client('s3')
+        
+        # First step is to initiate the multipart upload. Note that the storage
+        # class must be ONEZONE_IA.
+        create_response = client.create_multipart_upload(
+            Bucket='cubl-vtc',
+            Key=os.path.basename(f),
+            StorageClass='ONEZONE_IA')
+
+        # Second step is to upload the individual parts of the file. Parts are
+        # arbitrarily sized at 5MB -- if the size of the file is less than an
+        # even multiple of 5MB, the last part will be < 5MB.
+        #
+        # Note that for each part upload, the part number and corresponding ETag
+        # must be saved for the last step of the multipart upload process.
+        file_size = os.lstat(f).st_size
+        file_position = file_size
+        part_number = 1
+        parts = []
+
+        fio = io.FileIO(f)
+        while file_position > 0:
+            part = fio.read(UPLOAD_PART_SIZE)
+            upload_response = client.upload_part(
+                Body=part,
+                Bucket='cubl-vtc',
+                Key=os.path.basename(f),
+                PartNumber=part_number,
+                UploadId=create_response['UploadId'])
+            parts.append({'ETag': upload_response['ETag'], 'PartNumber': part_number})
+            part_number += 1
+            file_position -= UPLOAD_PART_SIZE
+        fio.close()
+
+        # Finally, complete the multipart upload
+        client.complete_multipart_upload(
+            Bucket='cubl-vtc',
+            Key=os.path.basename(f),
+            MultipartUpload={'Parts': parts},
+            UploadId=create_response['UploadId'])
+            
+    except:
+        
+        # If any problems are encountered along the way, publish an alert
         sns = boto3.client('sns')
         sns.publish(TopicArn='arn:aws:sns:us-west-2:735677975035:VTCBackupStatus',
-            Message='IOError: Unable to copy backup file to S3.',
+            Message='Error: Unable to copy backup file to S3',
             Subject='VTC Notification - Backup Status')
-
+        
 if __name__ == "__main__":
-
-    # Get the latest backup file from the backup directory
-    # The latest backup is the one with today's date-time stamp
-    flist = glob.glob(VTC_BACKUP + '*.vtcBackup')
-    if len(flist) <> 0:
-
-        # Find the backup file with today's date
-        for f in flist:
-            fstat = os.stat(f)
-            if date.fromtimestamp(fstat.st_ctime) == date.today():
-                break
     
-        copy_backup_to_s3(f)
+    # Get the latest backup file from the backup directory. The latest backup
+    # is the one with a create date-time stamp of today.
+    backup_dir = sys.argv[1]
+    flist = glob.glob('{}/*.vtcbackup'.format(os.path.normcase(backup_dir)))
+    for f in flist:
+        stat_info = os.stat(f)
+        if date.fromtimestamp(stat_info.st_ctime) == date.today():
+            break
+    
+    copy_backup_to_s3(f)
 
 # EOF
